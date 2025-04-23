c00bca629dd9a4947666d4c42a5d17e41ed0eaef
Vadim MM <vadimmas2006@gmail.com>
2025-04-23 23:09:13 +0300

    Added attack method and cost for all enemies

--- Enemy.java
+++ Enemy.java
@@ -6,10 +6,11 @@ abstract public class Enemy {
 	float melee_damage;
 	int cost;
 	
-	Enemy(String name, float health, float melee_damage){
+	Enemy(String name, float health, float melee_damage, int cost){
 		this.name = name;
 		this.health = health;
 		this.melee_damage = melee_damage;
+		this.cost = cost;
 	}
 	
 	void Attack(Player player) {
