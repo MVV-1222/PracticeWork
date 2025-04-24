279c3a58ccc2c994169a34e089872ccccb852922
Vadim MM <vadimmas2006@gmail.com>
2025-04-24 23:13:03 +0300

    Added access parameter

--- Weapon.java
+++ Weapon.java
@@ -3,9 +3,11 @@ package version1;
 public class Weapon {
 	String name;
 	int attack;
+	int access;
 	
-	Weapon(String name, int attack){
+	Weapon(String name, int attack, int access){
 		this.name = name;
 		this.attack = attack;
+		this.access = access; 
 	}
 }
