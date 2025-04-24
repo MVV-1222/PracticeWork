91fbdf95af21de3db1732724abb6063a6bc7168c
Vadim MM <vadimmas2006@gmail.com>
2025-04-24 23:43:42 +0300

    Added price parameter (constructor)

--- Weapon.java
+++ Weapon.java
@@ -6,9 +6,10 @@ public class Weapon {
 	int access;
 	int price;
 	
-	Weapon(String name, int attack, int access){
+	Weapon(String name, int attack, int access, int price){
 		this.name = name;
 		this.attack = attack;
 		this.access = access; 
+		this.price = price;
 	}
 }
